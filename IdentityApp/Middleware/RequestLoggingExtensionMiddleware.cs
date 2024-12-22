4b793a772ec8f78e1d22fdf62378d3a1c1dc0be5
Masa <j.zasadzki@interia.pl>
2024-12-22 01:32:25 +0100

    Added CorrelationId to Serilog request logging.

---
+++ IdentityApp/Middleware/RequestLoggingExtensionMiddleware.cs
@@ -0,0 +1,16 @@
+﻿using Serilog.Context;
+
+namespace IdentityApp.Middleware
+{
+    public class RequestLoggingExtensionMiddleware(
+        RequestDelegate _next)
+    {
+        public Task InvokeAsync(HttpContext context)
+        {
+            using(LogContext.PushProperty("CorrelationId", context.TraceIdentifier))
+            {
+                return _next(context);
+            }
+        }
+    }
+}
